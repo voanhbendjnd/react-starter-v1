8d0bc643a3900fc0eeecec7730bbf81c6cc4908d
Ta Khu Vu Khe Tu <voanhbendjndnd@gmail.com>
2025-03-09 00:02:52 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dns from 'dns'
+dns.setDefaultResultOrder('verbatim')
 
 // https://vite.dev/config/
 export default defineConfig({

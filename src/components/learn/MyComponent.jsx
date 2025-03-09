751fd56d4486bdf8f9539f15693ba23760179604
Ta Khu Vu Khe Tu <voanhbendjndnd@gmail.com>
2025-03-09 15:02:46 +0700

    31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,16 +1,26 @@
 import './style.css';
 // () => { }
 const MyComponent = () => {
+    // const ben = "123";
+    // const ben = 23;
+    // const ben = true; // boolean thi se khong in ra gi & undefined && null
+    // const ben = [1, 2, 3, 4];
+    const ben = {
+        name: "Phan Nhu Ngoc",
+        id: 123
+    }
+
   return (
       <>
           <div className="child"
               style={
               {
-                borderRadius: "10px"
+                borderRadius: "110px"
               }
           }
           >Anh Ben say hi</div>
-          <div>Hello !</div>
+          <div>{ben.name}</div>
+          <div>{ JSON.stringify(ben)}</div>
       </>
   );
 }
